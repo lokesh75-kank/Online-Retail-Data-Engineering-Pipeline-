9b5fa5d77d944035402746ce5ca5dc50615ee428
lokesh <kank.lokesh75@gmail.com>
2023-12-30 21:08:44 -0800

    did quality checks, created reports

--- dags/retail.py
+++ dags/retail.py
@@ -8,6 +8,12 @@ from astro.files import File
 from astro.sql.table import Table, Metadata
 from astro.constants import FileType
 
+
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
+
 @dag(
     start_date= datetime(2023,1,1),
     schedule=None,
@@ -48,6 +54,40 @@ def retail():
         
         @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
         def check_load(scan_name='check_load', checks_subpath='sources'):
+            from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+        check_load()
+
+        transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+        
+        @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+        def check_transform(scan_name='check_transform', checks_subpath='transform'):
+            from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+        report = DbtTaskGroup(
+            group_id='report',
+            project_config=DBT_PROJECT_CONFIG,
+            profile_config=DBT_CONFIG,
+            render_config=RenderConfig(
+                load_method=LoadMode.DBT_LS,
+                select=['path:models/report']
+            )
+    )
+        
+        @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+        def check_report(scan_name='check_report', checks_subpath='report'):
             from include.soda.check_function import check
 
         return check(scan_name, checks_subpath)
